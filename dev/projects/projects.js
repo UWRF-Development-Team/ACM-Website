049d0e55d066032def4bb213d4729f8ebdc520a0
jamesyjamesman <jameson.price@my.uwrf.edu>
2026-03-19 13:38:37 -0500

    Adds a slightly scuffed loading animation

--- dev/projects/projects.js
+++ dev/projects/projects.js
@@ -1,3 +1,5 @@
+// TODO: Extra credit: There's a slight vertical shift this causes I can't figure out. Fix it!
+
 window.addEventListener("DOMContentLoaded", () => {
     loadOne(document.querySelector("#content iframe"));
 })
